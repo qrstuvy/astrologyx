b9bc25df451cdf41694e659f91dabba50d249f66
qrstuvy <nguyen.vy11@gmail.com>
2022-12-23 14:04:42 -0500

    deployment

--- server.js
+++ server.js
@@ -28,8 +28,4 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const port = process.env.PORT || 3001;
-
-app.listen(port, function() {
-  console.log(`Express app running on port ${port}`);
-});
+module.exports = app
