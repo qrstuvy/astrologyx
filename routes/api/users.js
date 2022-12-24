e08864adf9b58bde7d2cda1515517e7b321313f1
qrstuvy <nguyen.vy11@gmail.com>
2022-12-23 19:46:28 -0500

    redeploy

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const usersCtrl = require('../../controllers/api/users');
+const ensureLoggedIn = require('./config/ensureLoggedIn');
 
 // POST /api/users
-router.post('/', usersCtrl.create);
+router.post('/', ensureLoggedIn, usersCtrl.create);
 // POST /api/users/login
-router.post('/login', usersCtrl.login);
+router.post('/login' usersCtrl.login);
 
 module.exports = router;
